fdd753976e2d923a38af4df791ef4414a87eebd5
gabr1234iel <gabrrrly@gmail.com>
2025-08-09 11:47:47 +0800

    sh scripts

--- src/nautilus-server/run.sh
+++ src/nautilus-server/run.sh
@@ -26,6 +26,7 @@ echo "127.0.0.64   api.weatherapi.com" >> /etc/hosts
 
 
 
+
 # == ATTENTION: code should be generated here that parses allowed_endpoints.yaml and populate domains here ===
 
 cat /etc/hosts
@@ -44,6 +45,7 @@ echo "$JSON_RESPONSE" | jq -r 'to_entries[] | "\(.key)=\(.value)"' > /tmp/kvpair
 # == ATTENTION: code should be generated here that added all hosts to forward traffic ===
 # Traffic-forwarder-block
 python3 /traffic_forwarder.py 127.0.0.64 443 3 8101 &
+
 
 
 # Listens on Local VSOCK Port 3000 and forwards to localhost 3000
